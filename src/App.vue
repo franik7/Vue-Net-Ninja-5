3e6e8bd0b1172afc9d6d556707aa1aa07583dac9
Frants <118231497+franik7@users.noreply.github.com>
2023-10-29 19:48:08 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"/>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results }, 
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000
-      this.isPlaying = true
-    }
+      this.isPlaying = true,
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime,
+      this.isPlaying = false,
+      this.showResults = true
+  }
   }
 }
 </script>
